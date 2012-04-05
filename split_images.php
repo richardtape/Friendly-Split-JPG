36f4df5779186b82bbb9bd969d2aa2ba1b20019f
Rich Tape <richard.tape@postdesk.com>
2012-04-06 00:28:49 +0100

    Added readme and tidied up

--- split_images.php
+++ split_images.php
@@ -1,69 +1,64 @@
 <?php
 
-function friendly_split_image( $jpeg_path = false, $width = 100, $height = 100 )
-{
+	/**
+	 * A function which splits a jpg image into smaller pieces (kind of like a jigsaw) and uploads those images to the
+	 * WordPress uploads folder.
+	 *
+	 * @param $jpeg_path : path to the jpg to split
+	 * @param $width : width of the required output images
+	 * @param @height : height of the required output images
+	 *
+	 * @return an array of paths to the image parts created (or false if it all goes horribly wrong)
+	 *
+	 * @package Friendly Split Image
+	 * @author iamfriendly
+	 * @version 1.0
+	 * @since 1.0
+	 */
 
-	if( $jpeg_path === false )
-		return false;
-		
-	$source 					= @imagecreatefromjpeg( $jpeg_path );
-	$source_width 		= imagesx( $source );
-	$source_height 		= imagesy( $source );
-	
-	$upload_dir = wp_upload_dir();
-	$upload_path = $upload_dir['path'];
-	
-	$img_parts = array();
-	
-	for( $col = 0; $col < $source_width / $width; $col++)
+	function friendly_wp_split_image( $jpeg_path = false, $width = 100, $height = 100 )
 	{
+	
+		if( $jpeg_path === false )
+			return false;
+			
+		$source 					= @imagecreatefromjpeg( $jpeg_path );
+		$source_width 		= imagesx( $source );
+		$source_height 		= imagesy( $source );
 		
-		for( $row = 0; $row < $source_height / $height; $row++)
+		$upload_dir = wp_upload_dir();
+		$upload_path = $upload_dir['path'];
+		
+		$img_parts = array();
+		
+		for( $col = 0; $col < $source_width / $width; $col++)
 		{
 			
-			$fn = sprintf( "img%02d_%02d.jpg", $col, $row );
-			
-			$col_class = $col +1;
-			$row_class = $row + 1;
-			
-			//echo '<img src="'.$fn.'" alt="" class="img-col-'.$col_class.' img-row-'.$row_class.'" />'; 
-			array_push($img_parts, $fn);
-			
-			$im = @imagecreatetruecolor( $width, $height );
-			imagecopyresized( $im, $source, 0, 0, $col * $width, $row * $height, $width, $height, $width, $height );
-			imagejpeg( $im, $fn );
-			imagedestroy( $im );
-			
+			for( $row = 0; $row < $source_height / $height; $row++)
+			{
+				
+				$fn = sprintf( "img%02d_%02d.jpg", $col, $row );
+				
+				$col_class = $col +1;
+				$row_class = $row + 1;
+				
+				array_push($img_parts, $fn);
+				
+				$im = @imagecreatetruecolor( $width, $height );
+				imagecopyresized( $im, $source, 0, 0, $col * $width, $row * $height, $width, $height, $width, $height );
+				imagejpeg( $im, $fn );
+				imagedestroy( $im );
+				
+			}
+				
 		}
-			
-	}
+		
+		return $img_parts;
 	
-	return $img_parts;
+	}/* friendly_wp_split_image() */
 
-}/* friendly_split_image() */
+$image_bits = friendly_wp_split_image( "image/dummy.jpg", 160, 160 );
 
-$image_bits = friendly_split_image( "image/dummy.jpg", 160, 160 );
-
-//var_dump($image_bits);
-
-echo "<img src='".$image_bits[0]."' alt='' />";
-echo "<img src='".$image_bits[4]."' alt='' />";
-echo "<img src='".$image_bits[8]."' alt='' />";
-echo "<img src='".$image_bits[12]."' alt='' />";
-
-echo "<img src='".$image_bits[1]."' alt='' />";
-echo "<img src='".$image_bits[5]."' alt='' />";
-echo "<img src='".$image_bits[9]."' alt='' />";
-echo "<img src='".$image_bits[13]."' alt='' />";
-
-echo "<img src='".$image_bits[2]."' alt='' />";
-echo "<img src='".$image_bits[6]."' alt='' />";
-echo "<img src='".$image_bits[10]."' alt='' />";
-echo "<img src='".$image_bits[14]."' alt='' />";
-
-echo "<img src='".$image_bits[3]."' alt='' />";
-echo "<img src='".$image_bits[7]."' alt='' />";
-echo "<img src='".$image_bits[11]."' alt='' />";
-echo "<img src='".$image_bits[15]."' alt='' />";
+var_dump($image_bits);
 
 ?>
